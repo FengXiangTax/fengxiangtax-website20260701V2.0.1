514e9065e068a8d233db4f2ceb03c9e15bb34343
fengxiangtax <zhanghaikang@fengxiangtax.com>
2026-07-02 09:56:25 +0800

    Add files via upload
    百度统计代码增加

---
+++ js/tracking.js
@@ -0,0 +1,11 @@
+/**
+ * 烽享财税 — 百度统计
+ * ID: f70e87de0b721af21107b215288a740b
+ */
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?f70e87de0b721af21107b215288a740b";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
